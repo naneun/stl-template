a0f64273f0b6d170866f8e009462077419b1e9bf
naneun <rmtlrksdp@naver.com>
2022-07-23 03:24:44 +0900

    refactor: remaster stack, queue c version

---
+++ c/queue_(2164).c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE (500000 + 5)
+
+struct _Queue {
+
+	int front;
+	int back;
+
+	int* buffer;
+};
+
+typedef struct _Queue Queue;
+
+Queue* create_queue() {
+	Queue* queue = (Queue*)malloc(sizeof(Queue));
+	queue->front = queue->back = 0;
+	queue->buffer = (int*)malloc(sizeof(int) * BUFFER_SIZE);
+	return queue;
+}
+
+int empty(Queue* queue) {
+	return queue->front == queue->back;
+}
+
+int full(Queue* queue) {
+	return (queue->back + 1) % BUFFER_SIZE == queue->front;
+}
+
+int size(Queue* queue) {
+	return (queue->back + BUFFER_SIZE - queue->front) % BUFFER_SIZE;
+}
+
+int front(Queue* queue) {
+	return queue->buffer[queue->front];
+}
+
+void push(Queue* queue, int value) {
+	if (full(queue)) {
+		return;
+	}
+	queue->buffer[(queue->back)++] = value;
+	queue->back %= BUFFER_SIZE;
+}
+
+void pop(Queue* queue) {
+	if (empty(queue)) {
+		return;
+	}
+	queue->front = (queue->front + 1) % BUFFER_SIZE;
+}
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+
+	Queue* queue = create_queue();
+	
+	int i;
+	for (i = 1; i <= n; ++i) {
+		push(queue, i);
+	}
+
+	while (size(queue) > 1) {
+			pop(queue);
+			push(queue, front(queue));
+			pop(queue);
+	}
+
+	printf("%d", front(queue));
+}
--- c/stack_(9012).c
+++ c/stack_(9012).c
@@ -1,32 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define BUFFER_SIZE		(50 + 5)
-#define STRING_LENGTH	(50 + 5)
-
-int strlen(const char* str) {
-	int i;
-	for (i = 0; str[i]; ++i);
-	return i;
-}
-
-int strcmp(const char* s1, const char* s2) {
-	int i;
-	for (i = 0; s1[i] || s2[i]; ++i) {
-		if (s1[i] != s2[i]) {
-			return s1[i] < s2[i] ? -1 : 1;
-		}
-	}
-	return 0;
-}
-
-void strcpy(char* s1, const char* s2) {
-	int i;
-	for (i = 0; s2[i]; ++i) {
-		s1[i] = s2[i];
-	}
-	s1[i] = 0;
-}
+#define BUFFER_SIZE (50 + 5)
+#define STRING_LENGTH (50 + 5)
 
 struct _Stack {
 
@@ -79,8 +55,8 @@ int main()
 		int i, length = strlen(str);
 		for (i = 0; i < length; ++i) {
 			push(stack, str[i]);
-			if (size(stack) > 1 
-				&& stack->buffer[stack->top - 2] == '(' 
+			if (size(stack) > 1
+				&& stack->buffer[stack->top - 2] == '('
 				&& stack->buffer[stack->top - 1] == ')') {
 
 				pop(stack);
